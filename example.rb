9506449c18182ae875460ea98f9c89bd768a41b8
Hallison Batista <email@hallison.dev.br>
2021-07-01 16:46:50 -0400

    Examples of how to use Drawght parser

---
+++ example.rb
@@ -0,0 +1,18 @@
+require "yaml"
+require_relative "lib/drawght"
+
+yaml = File.read("example.yaml")
+data = YAML.load(yaml)
+
+puts "Dataset", yaml
+Dir.glob "*.in" do |file|
+  template = File.read(file)
+  drawght = Drawght::Parser.new(template)
+  line = "-" * 78
+  puts line
+  puts drawght.parse_template("Template '#{file}' ({title})", data)
+  puts line
+  puts template
+  puts line
+  puts drawght.parse(data)
+end
