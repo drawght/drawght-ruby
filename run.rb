caabe1528108db4a9dcc786367700cadc018c26b
Hallison Batista <email@hallison.dev.br>
2025-12-22 19:45:06 -0300

    Script for tasks has been refactored

--- run.rb
+++ run.rb
@@ -1,45 +1,54 @@
-#! /bin/env ruby -Ilib:test
-
+#! /usr/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
-module Commands
-  def self.changelog
-    require "yaml"
+require "date"
+require "erb"
+require "pry-byebug" if ENV["DEBUG"]&.match? /\A(?:1|yes|true)\z/i
+require "yaml"
 
-    @changelog ||= YAML.safe_load_file "#{__dir__}/CHANGELOG.yaml", permitted_classes: [Symbol, Date]
-  end
+require_relative "#{__dir__}/lib/drawght"
 
-  def self.release
-    @release ||= Release.new **changelog.first
-  end
+module CommandRunner
+  RUNNABLES = %i[test versioning].freeze
 
   def self.test file = nil
     require "minitest/autorun"
-    require "pry-byebug"
-    require_relative "#{__dir__}/lib/drawght"
 
     filename_pattern = file || "#{__dir__}/test/*_test.rb"
 
-    Dir.glob filename_pattern do |file|
-      load file
+    Dir.glob filename_pattern do |pathname|
+      require pathname
     end
   end
 
   def self.versioning filename = nil
-    require "erb"
-
     template = "#{__dir__}/lib/drawght/version.rb.erb"
-    versioning = filename || template.sub(".erb", "")
+    versioning = filename || template.sub(".erb", '')
     templating = File.read template
     renderer = ERB.new templating, trim_mode: '-'
 
-    File.write versioning, renderer.result(release.instance_eval{ binding })
+    File.write versioning, renderer.result_with_hash(release.to_h)
   end
 
-  Release = Struct.new *changelog.first.keys.map(&:to_sym)
+  private_class_method
+
+  def self.changelog
+    @changelog ||= YAML.safe_load_file "#{__dir__}/CHANGELOG.yaml", permitted_classes: [Date]
+  end
+
+  def self.release
+    @release ||= Drawght::Release.new **changelog.first
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
   command = ARGV.shift&.to_sym
-  Commands.send command, *ARGV if Commands.methods(false).include? command
+
+  if CommandRunner::RUNNABLES.include? command
+    CommandRunner.send command, *ARGV
+  else
+    warn "Invalid command: '#{command}'. Available commands: #{CommandRunner::RUNNABLES.join ', '}"
+    exit 1
+  end
 end
