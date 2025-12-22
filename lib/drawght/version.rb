933376be171fb8332a48bcd7491884eb274e5896
Hallison Batista <email@hallison.dev.br>
2025-12-22 19:35:01 -0300

    Fixes to the development version file

--- lib/drawght/version.rb
+++ lib/drawght/version.rb
@@ -1,8 +1,9 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 module Drawght
   VERSION = "0.0.0-development"
-  RELEASE_DATE = Date.today.to_s
+  RELEASE_DATE = "1970-01-01"
   CHANGESET = [
     "Work In Progress",
   ]
